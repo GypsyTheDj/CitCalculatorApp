c0d42931e5acfd20787714a9c680d9ec5fdc3be1
Domains18 <gibsonsgibson88@gmail.com>
2023-02-19 23:27:05 +0300

    define calculator buttons

--- app/src/main/java/com/example/citcalculatorapp/calculatorButton.kt
+++ app/src/main/java/com/example/citcalculatorapp/calculatorButton.kt
@@ -1,4 +1,14 @@
 package com.example.citcalculatorapp
 
-class calculatorButton {
+import android.content.DialogInterface.OnClickListener
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun calculatorButton(
+	symbol: String,
+	modifier: Modifier,
+	onClickListener: () -> Unit
+) {
+	
 }
